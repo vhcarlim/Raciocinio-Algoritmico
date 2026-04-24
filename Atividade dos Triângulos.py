19dde9fb7a871eb0756d90807019ef839208d97f
Victor Hugo Carlim Pedroni dos Passos <vhcarlim080@gmail.com>
2026-04-24 13:04:54 -0300

    Update Atividade dos Triângulos.py

--- Atividade dos Triângulos.py
+++ Atividade dos Triângulos.py
@@ -1,8 +1,8 @@
-#Atividade dos Triângulos
-#Peça os três lados de uma figura. Primeiro, verifique se esses valores podem formar um triângulo.
-#Lembre-se: a soma de dois lados deve ser sempre maior que o terceiro.
-#Se for possível formar um triângulo, classifique-o: Equilátero: todos os lados têm o mesmo tamanho. Isósceles: dois lados têm o mesmo tamanho. Escaleno: todos os lados são diferentes.
-#Além disso, verifique se o triângulo é retângulo: Um triângulo é retângulo quando o quadrado do maior lado é igual à soma dos quadrados dos outros dois lados. Caso os valores não formem um triângulo, informe isso ao usuário.
+# Atividade dos Triângulos
+# Peça os três lados de uma figura. Primeiro, verifique se esses valores podem formar um triângulo.
+# Lembre-se: a soma de dois lados deve ser sempre maior que o terceiro.
+# Se for possível formar um triângulo, classifique-o: Equilátero: todos os lados têm o mesmo tamanho. Isósceles: dois lados têm o mesmo tamanho. Escaleno: todos os lados são diferentes.
+# Além disso, verifique se o triângulo é retângulo: Um triângulo é retângulo quando o quadrado do maior lado é igual à soma dos quadrados dos outros dois lados. Caso os valores não formem um triângulo, informe isso ao usuário.
 
 lado1 = float(input("Digite o primeiro lado: "))
 lado2 = float(input("Digite o segundo lado: "))
